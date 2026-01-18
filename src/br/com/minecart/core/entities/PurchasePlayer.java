f2720664fef7d57ac58119715265ce725fda30a7
Bruno Caitano <snowrunescape@snowdev.com.br>
2026-01-18 11:24:51 -0400

    feat: implement purchases functionality and refactor delivery confirmation method

---
+++ src/br/com/minecart/core/entities/PurchasePlayer.java
@@ -0,0 +1,12 @@
+package br.com.minecart.core.entities;
+
+public class PurchasePlayer {
+    public String player;
+    public String amount;
+
+    public PurchasePlayer(String player, String amount)
+    {
+        this.player = player;
+        this.amount = amount;
+    }
+}
